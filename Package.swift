8e8c5e53e456ff230baafe9027675cc02e02bb07
Philipp Dylong <creekpld@gmail.com>
2018-08-27 20:57:57 +0200

    update to swift-tools-version:4.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,19 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.1
 
 import PackageDescription
 
 let package = Package(
-    name: "Romanize"
+    name: "Romanize",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Romanize",
+            dependencies: [],
+            path: "Sources"),
+        ]
 )
